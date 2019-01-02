14f6d87ebdb9dd57c82500f057d8ac7e71cc97ba
Nasimul Haque <Nasimul_Haque@epam.com>
2019-01-02 13:40:27 +0530

    first commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No servers created';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created';
+  }
+}
